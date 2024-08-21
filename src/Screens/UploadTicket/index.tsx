abd2ff51d555c6034fd8e30938a7ffc9c53fb0b8
Rishabh6117 <rishikhaswin@gmail.com>
2024-08-21 17:24:51 +0530

    r

--- src/Screens/UploadTicket/index.tsx
+++ src/Screens/UploadTicket/index.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import styles from './style'
+import LinearGradient from 'react-native-linear-gradient'
+import { Colors } from '../../Utilities/Styles/colors'
+import { SizeBox } from '../../Utilities/Component/Helpers'
+import VectorIcon from '../../Utilities/Component/vectorIcons'
 
 const UploadTicket = () => {
+    const[upload,setUpload]= useState(false);
   return (
-    <View>
-      <Text>UploadTicket</Text>
+    <LinearGradient
+            colors={[Colors.LinearBlack, Colors.Linear]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1.3, y: 0.9 }}
+            style={styles.LinearConatiner}
+        >
+            <SizeBox size={10}/>
+      <Text style={styles.uploadticket}>Upload your ticket</Text>
+            <SizeBox size={10}/>
+    <Text style={styles.select}>Select the original file. You can choose which tickets to sell next.</Text>
+            <SizeBox size={10}/>
+            <View style={styles.vectortext}>
+                <VectorIcon groupName='MaterialIcons' name='add-link' size={25}/>
+    <Text style={styles.upload}>Upload your ticket</Text>
     </View>
+            <SizeBox size={10}/>
+            <View style={styles.vectortext}>
+                <VectorIcon groupName='AntDesign' name='plus' size={25}/>
+    <Text style={styles.upload}>Add another file</Text>
+    </View>
+            <SizeBox size={10}/>
+    <Text style={styles.upload}>Get help</Text>
+
+    <TouchableOpacity onPress={()=>setUpload(!upload)} style={styles.sytbtn}>
+                <Text style={styles.sell}>Next</Text>
+            </TouchableOpacity>
+    </LinearGradient>
   )
 }
 
 export default UploadTicket
 
-const styles = StyleSheet.create({})
--- src/Screens/UploadTicket/style.tsx
+++ src/Screens/UploadTicket/style.tsx
@@ -1,5 +1,54 @@
 import { StyleSheet } from "react-native";
+import fontFamily from "../../Utilities/Styles/fontFamily";
+import { Colors } from "../../Utilities/Styles/colors";
+import { moderateScaleVertical, width } from "../../Utilities/Styles/responsiveSize";
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    LinearConatiner: {flex: 1, paddingHorizontal: 20},
+    uploadticket:{
+        fontSize:20,
+        fontWeight:"700",
+        fontFamily:fontFamily.time_bold,
+        color:Colors.white,
+        textAlign:"center"
+    },
+    upload:{
+        fontSize:20,
+        fontWeight:"700",
+        fontFamily:fontFamily.time_bold,
+        color:Colors.white,
+        textAlign:"center",
+        paddingLeft:10
+    },
+    select:{
+        fontSize:16,
+        fontWeight:"500",
+        fontFamily:fontFamily.time_regular,
+        color:Colors.grey,
+        textAlign:"center"
+    },
+    sytbtn:{
+        borderWidth:1,
+        borderColor:Colors.grey,
+        height:moderateScaleVertical(50),
+        width:"60%",
+        alignItems:"center",
+        justifyContent:"center",
+        borderRadius:8,
+        alignSelf:"center",
+        position:"absolute",
+        bottom:7
+      },
+      sell:{
+        fontSize:20,
+        fontWeight:"400",
+        color:Colors.grey,
+        fontFamily:fontFamily.bold,
+      },
+      vectortext:{
+        flexDirection:"row",
+       alignSelf:"center"
+    }
+})
 
 export default styles
