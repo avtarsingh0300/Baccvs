bbe006c186157c149cb4025b2abdce9c34df925d
Siddharth7494 <99593978+Siddharth7494@users.noreply.github.com>
2024-09-04 18:13:47 +0530

    code is there

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -77,6 +77,7 @@ const Login = ({navigation}: any) => {
               placeholderTextColor={Colors.Pink}
               style={styles.input}
               value={username}
+              autoCapitalize="none"
               onChangeText={text => handleChangeUser(text)}
             />
           </View>
@@ -87,6 +88,7 @@ const Login = ({navigation}: any) => {
               placeholderTextColor={Colors.Pink}
               style={styles.input}
               value={password}
+              autoCapitalize="none"
               onChangeText={text => handleChangePass(text)}
             />
           </View>
